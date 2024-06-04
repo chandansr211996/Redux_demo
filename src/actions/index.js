81d0c85d6740eacddc5a8167076d3390577659e5
karthik <chandansr@datatemplate.com>
2024-06-04 14:14:58 +0530

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+// src/actions/index.js
+export const increment = () => {
+    return {
+        type: 'INCREMENT',
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    };
+};
+
+export const reset = () => {
+    return{
+        type: 'RESET'
+    }
+}
